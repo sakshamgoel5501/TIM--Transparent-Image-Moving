6d813c34912da14b1160f8aa36213bf8a31a7b9d
Saksham Goel <sakshamgoel5501@gmail.com>
2023-04-12 17:20:23 +0530

    Added files

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -29,10 +29,10 @@ export const getPost = async (req, res) => {
 
 export const createPost = async (req, res) => {
     // const { title, message, selectedFile, creator, tags } = req.body;
-    const { selectedFile, username, targetPosition} = req.body;
+    const { selectedFile1, username, targetPosition1, securityLevels, selectedFile2, targetPosition2, selectedFile3, targetPosition3, selectedFile4, targetPosition4, selectedFile5, targetPosition5} = req.body;
 
     // const newPostMessage = new PostMessage({ title, message, selectedFile, creator, tags })
-    const newPostMessage = new PostMessage({ selectedFile, username, targetPosition })
+    const newPostMessage = new PostMessage({ selectedFile1, username, targetPosition1, securityLevels, selectedFile2, targetPosition2, selectedFile3, targetPosition3, selectedFile4, targetPosition4, selectedFile5, targetPosition5 })
 
     try {
         await newPostMessage.save();
@@ -45,12 +45,12 @@ export const createPost = async (req, res) => {
 
 export const updatePost = async (req, res) => {
     const { id } = req.params;
-    const { username, selectedFile, targetPosition } = req.body;
+    const { username, selectedFile1, targetPosition1, securityLevels, selectedFile2, targetPosition2, selectedFile3, targetPosition3, selectedFile4, targetPosition4, selectedFile5, targetPosition5 } = req.body;
     // const { title, message, creator, selectedFile, tags } = req.body;
     
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`);
 
-    const updatedPost = { username, selectedFile, targetPosition, _id: id };
+    const updatedPost = { username, selectedFile1, targetPosition1, securityLevels, selectedFile2, targetPosition2, selectedFile3, targetPosition3, selectedFile4, targetPosition4, selectedFile5, targetPosition5, _id: id };
     // const updatedPost = { creator, title, message, tags, selectedFile, _id: id };
 
     await PostMessage.findByIdAndUpdate(id, updatedPost, { new: true });
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -2,8 +2,17 @@ import mongoose from 'mongoose';
 
 const postSchema = mongoose.Schema({
     username: String,
-    selectedFile: String,
-    targetPosition: Number,
+    selectedFile1: String,
+    targetPosition1: Number,
+    selectedFile2: String,
+    targetPosition2: Number,
+    selectedFile3: String,
+    targetPosition3: Number,
+    selectedFile4: String,
+    targetPosition4: Number,
+    selectedFile5: String,
+    targetPosition5: Number,
+    securityLevels: Number,
     createdAt: {
         type: Date,
         default: new Date()
