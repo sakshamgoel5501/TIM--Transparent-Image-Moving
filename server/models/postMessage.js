bfdc5ccbb61b587eda53dc43df7a4ec1a57be461
RohitTaparia <rohitktaparia@gmail.com>
2023-04-13 01:39:31 +0530

    Server changed

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -19,22 +19,6 @@ const postSchema = mongoose.Schema({
     },
 });
 
-// const postSchema = mongoose.Schema({
-//     title: String,
-//     message: String,
-//     creator: String,
-//     tags: [String],
-//     selectedFile: String,
-//     likeCount: {
-//         type: Number,
-//         default: 0,
-//     },
-//     createdAt: {
-//         type: Date,
-//         default: new Date(),
-//     },
-// });
-
 var PostMessage = mongoose.model('PostMessage', postSchema);
 
 export default PostMessage;
